04ef3d9e5f4195be634a8c787421020a7e815ae4
Ahmad Briezy Baihaqi <brezy.baihaqy@gmail.com>
2024-03-22 22:06:49 +0700

    add password validation min 8 character and try dummy icon. change existing favicon.ico in folder to new ico

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -36,6 +36,12 @@ export const Register = () => {
       return;
     }
 
+    // Validate password length
+    if (password.length < 8) {
+      toast.error("Password must be at least 8 characters long");
+      return;
+    }
+
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (!emailPattern.test(email)) {
